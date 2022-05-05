6be04f9e102282b0aca4381707465f02b78287c5
kaden530 <bro22001@byui.edu>
2022-05-05 08:01:46 -0600

    patch 1

--- Hilo class.py
+++ Hilo class.py
@@ -1,2 +1,7 @@
+import random 
 class Hilo:
     """ Face cards create a game of guessing whether the next card will be higher or lower in value than the previously drawn card."""
+    ##list of cards 
+
+    
+cards = []
--- director class.py
+++ director class.py
@@ -1,2 +1,64 @@
 class director:
     """The person who is playing Hilo decides whether to put all their points on the line or not by choosing to play and guess higher or lower."""
+    def __init__(self):
+
+
+        self.cards = []
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 300
+###
+            die = 
+            self.dice.append(die)
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Ask the user if they want to roll.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        roll_dice = input("Higher or lower? [h/l] ")
+#        self.is_playing = (roll_dice == "y")
+       
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return 
+
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            die.roll()
+            self.score += die.points 
+        self.total_score += self.score
+        self.score = 0
+
+    def do_outputs(self):
+        """Change this function to randomly select a card from the hilo list and determine if the card is higher or lower than the guess.
+        """
+        if not self.is_playing:
+            return
+        
+        values = ""
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            values += f"{die.value} "
+        
+
+        print(f"You rolled: {values}")
+        print(f"Your score is: {self.total_score}\n")
+        self.is_playing == (self.score > 0)
